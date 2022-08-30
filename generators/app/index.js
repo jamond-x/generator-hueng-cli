0f47e341d30c41cad204d50040545f92ca7521b7
jamond-x <x2592253600@163.com>
2022-08-30 15:45:19 +0800

    chore: init

---
+++ generators/app/index.js
@@ -0,0 +1,85 @@
+// generator 的核心入口
+const Generator = require('yeoman-generator');
+
+const ProjectMode = {
+  reactWithAntd: 'React + Ant design',
+  vue3With: 'Vue3 + Quasar2',
+};
+
+module.exports = class extends Generator {
+  prompting() {
+    this.log(`
+    
+😎欢迎使用hueng-cli!
+    
+    `);
+    return this.prompt([
+      {
+        name: 'project_name',
+        type: 'input',
+        message: '项目名称:',
+        default: 'a projet create by x-cli',
+      },
+      {
+        name: 'project_mode',
+        type: 'list',
+        choices: [
+          { name: ProjectMode.reactWithAntd, value: ProjectMode.reactWithAntd },
+          {
+            name: ProjectMode.vue3With,
+            value: ProjectMode.vue3With,
+            disabled: true,
+          },
+        ],
+      },
+    ]).then((ans) => {
+      this.ans = ans;
+    });
+  }
+
+  writing() {
+    const Templates = [
+      'public/favicon.ico',
+      'public/index.html',
+      'public/logo192.png',
+      'public/logo512.png',
+      'public/manifest.json',
+      'public/robots.txt',
+      'src/App.css',
+      'src/App.js',
+      'src/App.test.js',
+      'src/index.css',
+      'src/index.js',
+      'src/logo.svg',
+      'src/reportWebVitals.js',
+      'src/setupTests.js',
+      '.gitignore',
+      'package.json',
+      'README.md',
+    ];
+
+    // const template = this.templatePath('index.html');
+    // const output = this.destinationPath('index.html');
+    const context = { title: this.ans.project_name, ans: this.ans };
+
+    try {
+      Templates.forEach((item) => {
+        this.fs.copyTpl(
+          this.templatePath(item),
+          this.destinationPath(item),
+          context
+        );
+      });
+
+      this.log(`
+_____ _   _     _  _____   __
+| ____| \ | |   | |/ _ \ \ / /
+|  _| |  \| |_  | | | | \ V / 
+| |___| |\  | |_| | |_| || |  
+|_____|_| \_|\___/ \___/ |_|  
+`);
+    } catch {
+      this.log('wrong!');
+    }
+  }
+};
